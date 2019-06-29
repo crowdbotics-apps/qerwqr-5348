edc3495d04785ce86e1a4234a53ec2f7af9fa6a3
Crowdbotics <team@crowdbotics.com>
2019-06-29 13:38:17 +0000

    initial commit

---
+++ android/app/src/main/java/com/qerwqr_5348/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qerwqr_5348;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "qerwqr_5348";
+    }
+}
